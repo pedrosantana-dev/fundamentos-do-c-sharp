2a5373b7d57ff120bb8b62b8c0c58ae1fa4bc860
Pedro Santana <pedromanu27@gmail.com>
2023-09-22 10:01:33 -0300

    commit inicial

---
+++ MeuApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var texto = "Testando...";
+            Console.WriteLine(texto);
+        }
+    }
+}
