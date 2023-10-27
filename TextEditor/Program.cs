8545b14d548f80feb82ba654699f4366cc7b7247
Pedro Santana <pedromanu27@gmail.com>
2023-10-27 11:29:38 -0300

    TextEditor em C#. Menu e Editar

---
+++ TextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args) {
+        Menu();
+    }
+
+    static void Menu() {
+        Console.Clear();
+        Console.WriteLine("O que você deja fazer?");
+        Console.WriteLine("1 - Abrir um arquivo");
+        Console.WriteLine("2 - Criar um arquivo");
+        Console.WriteLine("0 - Sair");
+        short option = short.Parse(Console.ReadLine());
+
+        switch(option) {
+            case 0: System.Environment.Exit(0); break;
+            case 1: Abrir(); break;
+            case 2: Editar(); break;
+            default: Menu(); break;
+        }
+    }
+
+    static void Abrir() {}
+    static void Editar() {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo");
+        Console.WriteLine("----------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.Write(text);
+    }
+
+    static void Salvar(string text) {
+        Console.Clear();
+        Console.WriteLine("Qual caminho para salvar o arquivo?");
+    }
+}
